8b699ff559e904c4cdec3fcaedcc5f95e7aa4a95
Erik De Rijcke <derijcke.erik@gmail.com>
2017-08-22 15:49:58 +0200

    set buffer size

--- local/src/index.js
+++ local/src/index.js
@@ -46,6 +46,10 @@ function pushTestClientFrames (localClient, localRtcDcBuffer, grSurfaceProxy) {
           // send the buffer contents
           frameCounter++
           localRtcDcBuffer.rtcDcBufferProxy.syn(frameCounter)
+          // set size once
+          if (frameCounter === 1) {
+            localRtcDcBuffer.rtcDcBufferProxy.size(800, 600)
+          }
           frameHeader.writeUInt32BE(frameCounter, 0, false)
           const h264Nal = Buffer.concat([frameHeader, data])
           dataChannel.send(h264Nal.buffer.slice(h264Nal.byteOffset, h264Nal.byteOffset + h264Nal.byteLength))
