7439225e450720f233f897a99eabbcab28c64b4c
Erik De Rijcke <derijcke.erik@gmail.com>
2017-08-24 16:19:28 +0200

    make yuv shader work (not optimized yet)

--- browser/src/render/YUVSurfaceShader.js
+++ browser/src/render/YUVSurfaceShader.js
@@ -5,7 +5,6 @@ import ShaderCompiler from './ShaderCompiler'
 import { vertexQuad, fragmentYUV } from './ShaderSources'
 
 export default class YUVSurfaceShader {
-
   /**
    *
    * @param {WebGLRenderingContext} gl
@@ -14,7 +13,7 @@ export default class YUVSurfaceShader {
   static create (gl) {
     const program = this._initShaders(gl)
     const shaderArgs = this._initShaderArgs(gl, program)
-    const vertexBuffer = this._initBuffers(gl, shaderArgs)
+    const vertexBuffer = this._initBuffers(gl)
 
     return new YUVSurfaceShader(gl, vertexBuffer, shaderArgs, program)
   }
@@ -32,24 +31,25 @@ export default class YUVSurfaceShader {
   static _initShaderArgs (gl, program) {
     // find shader arguments
     const shaderArgs = {}
-    shaderArgs.u_projection = program.getUniformLocation('u_projection')
     shaderArgs.YTexture = program.getUniformLocation('YTexture')
     shaderArgs.UTexture = program.getUniformLocation('UTexture')
     shaderArgs.VTexture = program.getUniformLocation('VTexture')
+
+    shaderArgs.u_projection = program.getUniformLocation('u_projection')
     shaderArgs.u_transform = program.getUniformLocation('u_transform')
+
     shaderArgs.a_position = program.getAttributeLocation('a_position')
     gl.enableVertexAttribArray(shaderArgs.a_position)
     shaderArgs.a_texCoord = program.getAttributeLocation('a_texCoord')
     gl.enableVertexAttribArray(shaderArgs.a_texCoord)
+
     return shaderArgs
   }
 
-  static _initBuffers (gl, shaderArgs) {
+  static _initBuffers (gl) {
     // Create vertex buffer object.
     const vbo = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
-    gl.vertexAttribPointer(shaderArgs.a_position, 2, gl.FLOAT, false, 16, 0)
-    gl.vertexAttribPointer(shaderArgs.a_texCoord, 2, gl.FLOAT, false, 16, 8)
+
     return vbo
   }
 
@@ -73,19 +73,7 @@ export default class YUVSurfaceShader {
    * @param {Size} size
    */
   setSize (size) {
-    const gl = this.gl
-    gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer)
-    // TODO we could also do 3 subdata calls (probably faster as we have to transfer less data)
-    gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array([
-      // top right:
-      size.width, 0.0, 1.0, 0.0,
-      // bottom right:
-      size.width, size.height, 1.0, 1.0,
-      // bottom left:
-      0.0, size.height, 0.0, 1.0,
-      // top left:
-      0.0, 0.0, 0.0, 0.0
-    ]), this.gl.DYNAMIC_DRAW)
+    this.size = size
   }
 
   /**
@@ -103,16 +91,34 @@ export default class YUVSurfaceShader {
 
     gl.activeTexture(gl.TEXTURE1)
     gl.bindTexture(gl.TEXTURE_2D, textureU.texture)
-    gl.uniform1i(this.shaderArgs.UTexture, 0)
+    gl.uniform1i(this.shaderArgs.UTexture, 1)
 
     gl.activeTexture(gl.TEXTURE2)
     gl.bindTexture(gl.TEXTURE_2D, textureV.texture)
-    gl.uniform1i(this.shaderArgs.VTexture, 0)
+    gl.uniform1i(this.shaderArgs.VTexture, 2)
   }
 
   draw () {
     const gl = this.gl
     this.program.use()
-    gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4)
+    gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer)
+    // TODO we could also do 3 subdata calls (probably faster as we have to transfer less data)
+    gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array([
+      // top left:
+      0, 0, 0, 0,
+      // top right:
+      this.size.w, 0, 1, 0,
+      // bottom right:
+      this.size.w, this.size.h, 1, 1,
+      // bottom right:
+      this.size.w, this.size.h, 1, 1,
+      // bottom left:
+      0, this.size.h, 0, 1,
+      // top left:
+      0, 0, 0, 0
+    ]), this.gl.DYNAMIC_DRAW)
+    gl.vertexAttribPointer(this.shaderArgs.a_position, 2, gl.FLOAT, false, 16, 0)
+    gl.vertexAttribPointer(this.shaderArgs.a_texCoord, 2, gl.FLOAT, false, 16, 8)
+    gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 6)
   }
 }
