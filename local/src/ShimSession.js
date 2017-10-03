0be0a9871e0e5712f845abf7bc6f8e1434b5555c
Erik De Rijcke <derijcke.erik@gmail.com>
2017-10-03 14:03:10 +0200

    init native wayland display with basic shm formats

--- local/src/ShimSession.js
+++ local/src/ShimSession.js
@@ -2,17 +2,22 @@
 
 const LocalSession = require('./LocalSession')
 const {Display, Listener, Client} = require('wayland-server-bindings-runtime')
+const WlShmFormat = require('./protocol/wayland/WlShmFormat')
 
 module.exports = class ShimSession {
   static create (request, socket, head) {
     const wlDisplay = Display.create()
+    wlDisplay.addShmFormat(WlShmFormat.xrgb8888)
+    wlDisplay.addShmFormat(WlShmFormat.argb8888)
+    wlDisplay.initShm()
     const waylandSocket = wlDisplay.addSocketAuto()
     console.log('Created wayland socket: ' + waylandSocket)
-    wlDisplay.initShm()
 
     return LocalSession.create(request, socket, head, wlDisplay).then((localSession) => {
       const shimSession = new ShimSession(localSession, wlDisplay)
-      wlDisplay.addClientCreatedListener(Listener.create(shimSession.onClientCreated.bind(shimSession)))
+      const listener = Listener.create(shimSession.onClientCreated.bind(shimSession))
+      wlDisplay.addClientCreatedListener(listener)
+      shimSession.startLoop()
       return shimSession
     })
   }
@@ -35,7 +40,7 @@ module.exports = class ShimSession {
 
     this.stopLoop()
     this.localSession.createConnection(client).then((localClient) => {
-      // client can now safely bind to shim globals
+      // new connection with browser set up, client can now safely bind to shim globals
       this.startLoop()
     }).catch((error) => {
       console.error(error)
@@ -50,10 +55,14 @@ module.exports = class ShimSession {
 
   _doLoop () {
     setImmediate(() => {
-      if (this._loop) {
-        this.wlDisplay.flushClients()
-        this.wlDisplay.eventLoop.dispatch(0)
-        this._doLoop()
+      try {
+        if (this._loop) {
+          this.wlDisplay.flushClients()
+          this.wlDisplay.eventLoop.dispatch(-1)
+          this._doLoop()
+        }
+      } catch (error) {
+        console.error(error)
       }
     })
   }
