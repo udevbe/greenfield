2fe609a321601e27754ba2b931b2ea9f930b82f6
Erik De Rijcke <derijcke.erik@gmail.com>
2017-10-18 13:48:46 +0200

    dont' assume app source caps + add [bogus] framerate to quickly init a proper sps/pps

--- local/src/H264Encoder.js
+++ local/src/H264Encoder.js
@@ -4,16 +4,13 @@ const gstreamer = require('gstreamer-superficial')
 
 module.exports = class H264Encoder {
   static create (width, height) {
-    const pipeline = new gstreamer.Pipeline('appsrc name=source is-live=true ! ' +
+    const pipeline = new gstreamer.Pipeline('appsrc name=source ! ' +
       'videoconvert ! video/x-raw,format=I420,width=' + width + ',height=' + height + ' !' +
       'x264enc byte-stream=true key-int-max=30 pass=pass1 tune=zerolatency threads=2 ip-factor=2 speed-preset=veryfast intra-refresh=0 qp-max=43 !' +
-      'video/x-h264,profile=constrained-baseline,stream-format=byte-stream,framerate=0/1 !' +
-      'appsink max-buffers=1 drop=true name=sink')
+      'video/x-h264,profile=constrained-baseline,stream-format=byte-stream,framerate=30/1 !' +
+      'appsink name=sink')
     const appsink = pipeline.findChild('sink')
     const appsrc = pipeline.findChild('source')
-    appsrc.setCapsFromString('video/x-raw,format=xRGB,width=' + width + ',height=' + height + ',bpp=32,depth=32,framerate=0/1')
-    pipeline.play()
-
     return new H264Encoder(pipeline, appsink, appsrc, width, height)
   }
 
