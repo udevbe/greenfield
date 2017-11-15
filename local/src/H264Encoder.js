dc3fc260a3ef0720a8c88ee5813a69e1632645e1
Erik De Rijcke <derijcke.erik@gmail.com>
2017-11-15 21:33:17 +0100

    scale buffer to multiple of 2, neede by x264encoder

--- local/src/H264Encoder.js
+++ local/src/H264Encoder.js
@@ -4,8 +4,10 @@ const gstreamer = require('gstreamer-superficial')
 
 module.exports = class H264Encoder {
   static create (width, height) {
+    const scaledWidth = width - (width % 2)
+    const scaledHeight = height - (height % 2)
     const pipeline = new gstreamer.Pipeline('appsrc name=source ! ' +
-      'videoconvert ! video/x-raw,format=I420,width=' + width + ',height=' + height + ' ! ' +
+      'videoconvert ! videoscale !video/x-raw,format=I420,width=' + scaledWidth + ',height=' + scaledHeight + ' ! ' +
       'x264enc byte-stream=true key-int-max=1 pass=pass1 tune=zerolatency threads=1 ip-factor=2 speed-preset=veryfast intra-refresh=0 qp-max=43 !' +
       'video/x-h264,profile=constrained-baseline,stream-format=byte-stream,framerate=30/1 ! ' +
       'appsink name=sink')
