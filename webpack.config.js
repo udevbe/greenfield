50a039f821c4955ea8f67a02f39e9ac3a0f90ea4
Erik De Rijcke <derijcke.erik@gmail.com>
2018-04-01 20:57:47 +0200

    google closure compiler experiment

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path')
+// const ClosureCompilerPlugin = require('webpack-closure-compiler')
 
 module.exports = {
+  mode: 'none',
   entry: './browser/src/index.js',
   output: {
     path: path.resolve(__dirname, 'browser/public'),
     filename: 'browser.bundle.js'
   }
+  // enable if you want a minimized build.
+  // , plugins: [
+  //   new ClosureCompilerPlugin({
+  //     compiler: {
+  //       create_source_map: true,
+  //       language_in: 'ECMASCRIPT6_STRICT',
+  //       language_out: 'ECMASCRIPT6_STRICT',
+  //       compilation_level: 'ADVANCED'
+  //     }
+  //   })
+  // ]
 }
