3f479d79eb7caf9d5224562e000e05970ad20716
Erik De Rijcke <derijcke.erik@gmail.com>
2018-04-17 14:54:06 +0200

    replace printf with echo, travis doesn't like it

--- build-dist.sh
+++ build-dist.sh
@@ -16,8 +16,8 @@ function build_dist {
     rm -rf "node_modules"
     npm install
     patch -p0 < ${FIX_WRTC_REQUIRE_PATCH}
-    pkg -t node9-linux-x64 ./package.json --output ./dist/${RUN_GF_FILE}
-    printf '#!/usr/bin/env bash\n\nNODE_ENV=production ./greenfield' > ./dist/${RUN_GF_PROD_FILE}
+    pkg -t node9-linux-x64 ./package.json --output ${DIST_DIR}/${RUN_GF_FILE}
+    echo -e '#!/usr/bin/env bash\n\nNODE_ENV=production ./greenfield' > ${DIST_DIR}/${RUN_GF_PROD_FILE}
     chmod +x ./dist/${RUN_GF_PROD_FILE}
 }
 
