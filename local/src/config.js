2084a3f2b401a713ca2b146bc6da47a836794e0d
Erik De Rijcke <derijcke.erik@gmail.com>
2018-04-25 21:59:26 +0200

    print out config source

--- local/src/config.js
+++ local/src/config.js
@@ -4,20 +4,23 @@ const path = require('path')
 const fs = require('fs')
 
 let configFile = process.argv[2]
-let config
+
+let configPath
 try {
   if (configFile) {
-    config = JSON.parse(fs.readFileSync(configFile.startsWith('/') ? configFile : `${process.cwd()}/${configFile}`))
+    configPath = configFile.startsWith('/') ? configFile : `${process.cwd()}/${configFile}`
   } else {
-    config = JSON.parse(fs.readFileSync(path.join(__dirname, 'DefaultConfig.json')))
+    configPath = path.join(__dirname, 'DefaultConfig.json')
   }
+  const config = JSON.parse(fs.readFileSync(configPath))
+
+  console.log(` --- Loaded configuration: ${configPath} ---`)
+  console.log('')
+  console.log(config)
+  console.log('')
+
+  module.exports = config
 } catch (error) {
   console.trace(error)
   process.exit()
 }
-
-console.log(' --- Loaded configuration ---')
-console.log(config)
-console.log(' ---------------------------- ')
-
-module.exports = config
