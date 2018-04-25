cec1553071a5b2a92fab70324f331880ae13ef1e
Erik De Rijcke <derijcke.erik@gmail.com>
2018-04-25 21:44:30 +0200

    add simple config parse check

--- local/src/config.js
+++ local/src/config.js
@@ -5,10 +5,15 @@ const fs = require('fs')
 
 let configFile = process.argv[2]
 let config
-if (configFile) {
-  config = JSON.parse(fs.readFileSync(configFile.startsWith('/') ? configFile : `${process.cwd()}/${configFile}`))
-} else {
-  config = JSON.parse(fs.readFileSync(path.join(__dirname, 'DefaultConfig.json')))
+try {
+  if (configFile) {
+    config = JSON.parse(fs.readFileSync(configFile.startsWith('/') ? configFile : `${process.cwd()}/${configFile}`))
+  } else {
+    config = JSON.parse(fs.readFileSync(path.join(__dirname, 'DefaultConfig.json')))
+  }
+} catch (error) {
+  console.trace(error)
+  process.exit()
 }
 
 console.log(' --- Loaded configuration ---')
