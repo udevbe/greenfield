e6202496c738bb2e5783069ef3dee8c036beaba0
Erik De Rijcke <derijcke.erik@gmail.com>
2018-10-31 23:01:14 +0100

    remove closure compiler

--- compositor/web/build.config/webpack.config.prod.js
+++ compositor/web/build.config/webpack.config.prod.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const ClosureCompilerPlugin = require('webpack-closure-compiler')
 const merge = require('webpack-merge')
 
 const baseConfig = require('./webpack.config.base')
@@ -13,15 +12,6 @@ const base = baseConfig(appBundle, buildDir, false)
 const prod = {
   mode: 'none',
   plugins: [
-    new ClosureCompilerPlugin({
-      compiler: {
-        create_source_map: true,
-        language_in: 'ECMASCRIPT6_STRICT',
-        language_out: 'ECMASCRIPT6_STRICT',
-        compilation_level: 'ADVANCED',
-        define: 'DEBUG=false'
-      }
-    }),
     new CleanWebpackPlugin([buildDir], {
       root: path.resolve(__dirname, '..')
     }),
