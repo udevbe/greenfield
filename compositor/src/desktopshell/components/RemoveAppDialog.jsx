d5c8447fade8a7bcd29c7569a2ea1b0d52aa3efa
Erik De Rijcke <derijcke.erik@gmail.com>
2019-05-18 13:28:17 +0200

    add missing copyright header

--- compositor/src/desktopshell/components/RemoveAppDialog.jsx
+++ compositor/src/desktopshell/components/RemoveAppDialog.jsx
@@ -1,10 +1,26 @@
+// Copyright 2019 Erik De Rijcke
+//
+// This file is part of Greenfield.
+//
+// Greenfield is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Greenfield is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Greenfield.  If not, see <https://www.gnu.org/licenses/>.
+
 'use strict'
 
 import React from 'react'
 import Dialog from '@material-ui/core/es/Dialog'
 import DialogContent from '@material-ui/core/es/DialogContent'
 import DialogContentText from '@material-ui/core/es/DialogContentText'
-import withMobileDialog from '@material-ui/core/withMobileDialog'
 import DialogActions from '@material-ui/core/es/DialogActions'
 import Button from '@material-ui/core/es/Button'
 import Slide from '@material-ui/core/es/Slide'
