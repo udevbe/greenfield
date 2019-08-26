bfc159c2a6bc1203a9f89f5d9c6ebb3370c85b1e
Erik De Rijcke <derijcke.erik@gmail.com>
2019-08-26 16:20:28 +0200

    fix bad c/p

--- ext/build_gst_with_nvenc.sh
+++ ext/build_gst_with_nvenc.sh
@@ -52,7 +52,7 @@ cd ..
 
 cd gst-plugins-bad
 git checkout $BRANCH
-./configure --disable-gtk-doc --enable-orc NVENCODE_CFLAGS="-I/usr/local/nvidia/include" NVENCODE_LIBS="-L/usr/local/nvidia/lib64/" --with-cuda-prefix="/usr"
+./autogen.sh --disable-gtk-doc --enable-orc NVENCODE_CFLAGS="-I/usr/local/nvidia/include" NVENCODE_LIBS="-L/usr/local/nvidia/lib64/" --with-cuda-prefix="/usr"
 make
 make install
 cd ..
