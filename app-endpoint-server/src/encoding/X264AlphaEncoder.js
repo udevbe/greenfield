1a8bf2f8af265d96f3da7d568a0d45653f4c989c
Erik De Rijcke <derijcke.erik@gmail.com>
2020-08-19 20:55:44 +0200

    fix remote endpoint not working with node >=v14.x

--- app-endpoint-server/src/encoding/X264AlphaEncoder.js
+++ app-endpoint-server/src/encoding/X264AlphaEncoder.js
@@ -77,23 +77,24 @@ class X264AlphaEncoder {
   }
 
   /**
-   * @param {Buffer}pixelBuffer
+   * @param {Object}pixelBuffer
    * @param {number}wlShmFormat
    * @param {number}x
    * @param {number}y
    * @param {number}width
    * @param {number}height
+   * @param {number}stride
    * @return {Promise<EncodedFrameFragment>}
    * @private
    */
-  async _encodeFragment (pixelBuffer, wlShmFormat, x, y, width, height) {
+  async _encodeFragment (pixelBuffer, wlShmFormat, x, y, width, height, stride) {
     const gstBufferFormat = gstFormats[wlShmFormat]
 
     const encodingPromise = new Promise(resolve => {
       this._alpha = null
       this._opaque = null
       this._encodingResolve = resolve
-      appEndpointNative.encodeBuffer(this._encodingContext, pixelBuffer, gstBufferFormat, width, height)
+      appEndpointNative.encodeBuffer(this._encodingContext, pixelBuffer, gstBufferFormat, width, height, stride)
     })
 
     await encodingPromise
@@ -101,7 +102,7 @@ class X264AlphaEncoder {
   }
 
   /*
-   * @param {Buffer}pixelBuffer
+   * @param {Object}pixelBuffer
    * @param {number}wlShmFormat
    * @param {number}bufferWidth
    * @param {number}bufferHeight
@@ -109,11 +110,11 @@ class X264AlphaEncoder {
    * @return {Promise<EncodedFrame>}
    * @override
    */
-  async encodeBuffer (pixelBuffer, wlShmFormat, bufferWidth, bufferHeight, serial) {
+  async encodeBuffer (pixelBuffer, wlShmFormat, bufferWidth, bufferHeight, bufferStride, serial) {
     let encodingOptions = 0
     encodingOptions = EncodingOptions.enableSplitAlpha(encodingOptions)
     encodingOptions = EncodingOptions.enableFullFrame(encodingOptions)
-    const encodedFrameFragment = await this._encodeFragment(pixelBuffer, wlShmFormat, 0, 0, bufferWidth, bufferHeight)
+    const encodedFrameFragment = await this._encodeFragment(pixelBuffer, wlShmFormat, 0, 0, bufferWidth, bufferHeight, bufferStride)
     return EncodedFrame.create(serial, h264, encodingOptions, bufferWidth, bufferHeight, [encodedFrameFragment])
   }
 }
