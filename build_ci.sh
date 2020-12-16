2c228d17dc215dd4126dec96cdf70a91c286575c
Erik De Rijcke <derijcke.erik@gmail.com>
2020-12-16 08:38:34 +0100

    fix ci build file

--- build_ci.sh
+++ build_ci.sh
@@ -9,13 +9,13 @@ build () {
     printf "======[ BUILD $component STARTED ]======\n"
     pushd ${component}
     yarn install --frozen-lockfile
+    yarn build
     popd
     printf "======[ BUILD $component DONE ]======\n"
 }
 
 printf "======[ STARTING BUILDS ]=====\n"
 build component=compositor-module
-build component=app-endpoint-native
 build component=app-endpoint-server
 printf "======[ ALL BUILDS DONE ]======\n"
 
