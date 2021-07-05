66b1ca59343ba9ea7686b2629e77a4ec05747357
Peter Leurs <peter@pfoe.be>
2021-07-05 12:01:49 +0200

    Safari compatibility: window.screen.orientation
    Safari does not know about window.screen.orientation, for now hardcode this

--- compositor-module/src/browser/capabilities.ts
+++ compositor-module/src/browser/capabilities.ts
@@ -1,5 +1,15 @@
+let orientationType
+
+if (window.screen.orientation) {
+  orientationType = window.screen.orientation.type
+} else {
+  // Safari doesn't undestand window.screen.orientation, for now hardcode this
+  // TODO: fix this so ipad's can properly work
+  orientationType = "landscape-primary"
+}
+
 export const capabilities = {
   hasTouch: 'ontouchstart' in document.documentElement,
   userAgent: window.navigator.userAgent,
-  orientationType: window.screen.orientation.type,
+  orientationType
 }
