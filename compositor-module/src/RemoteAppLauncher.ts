83369b11b191b54dde9c0ad6a43840eea1e27e59
Erik De Rijcke <derijcke.erik@gmail.com>
2021-07-05 12:53:11 +0200

    don't retry when connecting to proxy

--- compositor-module/src/RemoteAppLauncher.ts
+++ compositor-module/src/RemoteAppLauncher.ts
@@ -34,11 +34,6 @@ export default class RemoteAppLauncher implements CompositorProxyConnector {
   }
 
   async connectTo(appEndpointURL: URL): Promise<Client> {
-    try {
-      return this.remoteSocket.onWebSocket(new WebSocket(appEndpointURL.href))
-    } catch (error) {
-      await new Promise((resolve) => setTimeout(resolve, 1000))
-      return this.remoteSocket.onWebSocket(new WebSocket(appEndpointURL.href))
-    }
+    return this.remoteSocket.onWebSocket(new WebSocket(appEndpointURL.href))
   }
 }
