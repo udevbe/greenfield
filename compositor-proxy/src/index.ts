f4eb661e99717d2423a8a8964746daa53141a5d2
Erik De Rijcke <derijcke.erik@gmail.com>
2023-06-20 19:00:42 +0200

    migrate (nearly) all interaction to node websockets

--- compositor-proxy/src/index.ts
+++ compositor-proxy/src/index.ts
@@ -1,8 +1,9 @@
 export { createLogger } from './Logger'
 export { initSurfaceBufferEncoding } from './SurfaceBufferEncoding'
 export { createApp } from './App'
-export { closeAllProxySessions, createProxySession, findProxySessionByCompositorSessionId } from './ProxySession'
+export { closeAllProxySessions, createProxySession, ProxySession } from './ProxySession'
 export { launchApplication, NativeAppContext } from './NativeAppContext'
+export { Configschema } from './@types/config'
 
 // TODO deal with config
 // TODO deal with process args
