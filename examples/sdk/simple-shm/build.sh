b84b0116ef28d73c232012e24de73f39d9ac538b
Erik De Rijcke <derijcke.erik@gmail.com>
2023-09-22 16:37:40 +0200

    more wip

--- examples/sdk/simple-shm/build.sh
+++ examples/sdk/simple-shm/build.sh
@@ -4,12 +4,12 @@ set -e
 source ../../../sdk/websdk_env.sh
 
 #generate sources
-wayland-scanner client-header < "$_SDK_DIR/sysrootlibs/wayland/xdg-shell.xml" > src/xdg-shell-client-protocol.h
-wayland-scanner private-code < "$_SDK_DIR/sysrootlibs/wayland/xdg-shell.xml" > src/xdg-shell-client-protocol.c
+wayland-scanner client-header < "$(pkg-config wayland-protocols --variable=pkgdatadir)/stable/xdg-shell/xdg-shell.xml" > src/xdg-shell-client-protocol.h
+wayland-scanner private-code < "$(pkg-config wayland-protocols --variable=pkgdatadir)/stable/xdg-shell/xdg-shell.xml" > src/xdg-shell-client-protocol.c
 
 #build
 mkdir -p build
-WAYLAND_FLAGS=$(gfpkg-config --cflags --libs --static wayland-client)
+WAYLAND_FLAGS=$(pkg-config --cflags --libs --static wayland-client)
 EXTRA_CFLAGS="-O3 -flto -Wl,-u,htons, -Wl,-u,ntohs -msimd128 -msse -includexmmintrin.h"
 
-gfcc src/simple-shm.c src/xdg-shell-client-protocol.c $EXTRA_CFLAGS $WAYLAND_FLAGS -o build/index.html
+gfcc src/simple-shm.c src/xdg-shell-client-protocol.c $EXTRA_CFLAGS $WAYLAND_FLAGS -o build/simple-shm
