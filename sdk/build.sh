e3a8a3b42611e4fb4bcb4c381a912ddeb2afe5eb
Erik De Rijcke <derijcke.erik@gmail.com>
2024-10-10 19:06:29 +0200

    gtk wasm build wip

--- sdk/build.sh
+++ sdk/build.sh
@@ -10,41 +10,50 @@ popd
 
 _SDK_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
-printf '%s\n' "[constants]" "toolchain = '$_SDK_DIR/emsdk/upstream/emscripten'" > "$_SDK_DIR/sysrootlibs/emscripten-toolchain.ini"
-printf '%s\n' "[constants]" "greenfield_sdk = '$_SDK_DIR'" > "$_SDK_DIR/toolkit/meson-gf-toolchain.ini"
+cat > "$_SDK_DIR/sysrootlibs/emscripten-toolchain.ini" <<- EOF
+[constants]
+toolchain = '$_SDK_DIR/emsdk/upstream/emscripten'
+sys_root = '$_SDK_DIR/sysroot'
+pkg_config_libdir = '$_SDK_DIR/sysroot'
+pkg_config_path = '$_SDK_DIR/sysroot/lib/pkgconfig:$_SDK_DIR/sysroot/share/pkgconfig'
+EOF
 
-./sysrootlibs/expat/build.sh
-./sysrootlibs/libffi/build.sh
-./sysrootlibs/wayland/build.sh
-./sysrootlibs/wayland-protocols/build.sh
-./sysrootlibs/pixman/build.sh
-./sysrootlibs/zlib/build.sh
-./sysrootlibs/png/build.sh
-./sysrootlibs/xml2/build.sh
-./sysrootlibs/xkeyboard-config/build.sh
-./sysrootlibs/xkbcommon/build.sh
-./sysrootlibs/icu/build.sh
-./sysrootlibs/harfbuzz/build_nofreetype_nocairo_noglib.sh
-./sysrootlibs/freetype/build.sh
-./sysrootlibs/harfbuzz/build_freetype_nocairo_noglib.sh
-./sysrootlibs/fontconfig/build.sh
-./sysrootlibs/pcre2/build.sh
-./sysrootlibs/glib/build.sh
-./sysrootlibs/jpeg/build.sh
-./sysrootlibs/cairo/build.sh
-./sysrootlibs/harfbuzz/build.sh
-./sysrootlibs/upoll/build.sh
-./sysrootlibs/cygepoll/build.sh
-./sysrootlibs/fribidi/build.sh
-./sysrootlibs/tiff4/build.sh
-./sysrootlibs/pango/build.sh
-./sysrootlibs/gdk-pixbuf/build.sh
-./sysrootlibs/egl-stub/build.sh
-./sysrootlibs/glesv2-stub/build.sh
+cat > "$_SDK_DIR/toolkit/meson-gf-toolchain.ini" <<- EOF
+[constants]
+greenfield_sdk = '$_SDK_DIR'
+EOF
+
+#./sysrootlibs/expat/build.sh
+#./sysrootlibs/libffi/build.sh
+#./sysrootlibs/wayland/build.sh
+#./sysrootlibs/wayland-protocols/build.sh
+#./sysrootlibs/pixman/build.sh
+#./sysrootlibs/zlib/build.sh
+#./sysrootlibs/png/build.sh
+#./sysrootlibs/xml2/build.sh
+#./sysrootlibs/xkeyboard-config/build.sh
+#./sysrootlibs/xkbcommon/build.sh
+#./sysrootlibs/icu/build.sh
+#./sysrootlibs/harfbuzz/build_nofreetype_nocairo_noglib.sh
+#./sysrootlibs/freetype/build.sh
+#./sysrootlibs/harfbuzz/build_freetype_nocairo_noglib.sh
+#./sysrootlibs/fontconfig/build.sh
+#./sysrootlibs/pcre2/build.sh
+#./sysrootlibs/glib/build.sh
+#./sysrootlibs/jpeg/build.sh
+#./sysrootlibs/cairo/build.sh
+#./sysrootlibs/harfbuzz/build.sh
+#./sysrootlibs/upoll/build.sh
+#./sysrootlibs/cygepoll/build.sh
+#./sysrootlibs/fribidi/build.sh
+#./sysrootlibs/tiff4/build.sh
+#./sysrootlibs/pango/build.sh
+#./sysrootlibs/gdk-pixbuf/build.sh
+#./sysrootlibs/egl-stub/build.sh
+#./sysrootlibs/glesv2-stub/build.sh
 ./sysrootlibs/epoxy-emscripten/build.sh
 ./sysrootlibs/graphene/build.sh
 ./sysrootlibs/sass/build.sh
 ./sysrootlibs/sassc/build.sh
 ./sysrootlibs/uapi-stub/build.sh
 ./sysrootlibs/gtk4/build.sh
-
--- sdk/sysrootlibs/cairo/build.sh
+++ sdk/sysrootlibs/cairo/build.sh
@@ -21,8 +21,6 @@ ensure_repo() {
 build() {
     ensure_repo
     source ../../emsdk/emsdk_env.sh
-    export PKG_CONFIG_PATH="$_SDK_DIR/sysroot/lib/pkgconfig:$_SDK_DIR/sysroot/share/pkgconfig"
-    export PKG_CONFIG_LIBDIR="$_SDK_DIR/sysroot"
     pushd repo
     	meson setup --wipe build/ --cross-file "${_SDK_DIR}/sysrootlibs/emscripten-toolchain.ini" --cross-file "${_SDK_DIR}/sysrootlibs/emscripten-build.ini" -Dprefix="${_SDK_DIR}/sysroot" --pkg-config-path="${_SDK_DIR}/sysroot/lib/pkgconfig:${_SDK_DIR}/sysroot/share/pkgconfig" \
     	  -Ddwrite=disabled -Dpng=enabled -Dquartz=disabled -Dtee=disabled -Dxcb=disabled -Dxlib=disabled -Dxlib-xcb=disabled -Dzlib=enabled \
