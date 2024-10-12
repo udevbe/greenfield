cc196d55fd4ac9d2f3ba281cbacad98b9e48088c
Erik De Rijcke <derijcke.erik@gmail.com>
2024-10-12 21:47:19 +0200

    gtk wasm build wip

--- sdk/build.sh
+++ sdk/build.sh
@@ -20,6 +20,7 @@ pkg_config_libdir = '$_SDK_DIR/sysroot'
 
 [built-in options]
 pkg_config_path = '$_SDK_DIR/sysroot/lib/pkgconfig:$_SDK_DIR/sysroot/share/pkgconfig'
+prefix = '$_SDK_DIR/sysroot'
 EOF
 
 cat > "$_SDK_DIR/toolkit/meson-gf-toolchain.ini" <<- EOF
@@ -27,37 +28,37 @@ cat > "$_SDK_DIR/toolkit/meson-gf-toolchain.ini" <<- EOF
 greenfield_sdk = '$_SDK_DIR'
 EOF
 
-#./sysrootlibs/expat/build.sh
-#./sysrootlibs/libffi/build.sh
-#./sysrootlibs/wayland/build.sh
-#./sysrootlibs/wayland-protocols/build.sh
-#./sysrootlibs/pixman/build.sh
-#./sysrootlibs/zlib/build.sh
-#./sysrootlibs/png/build.sh
-#./sysrootlibs/xml2/build.sh
-#./sysrootlibs/xkeyboard-config/build.sh
-#./sysrootlibs/xkbcommon/build.sh
-#./sysrootlibs/icu/build.sh
-#./sysrootlibs/harfbuzz/build_nofreetype_nocairo_noglib.sh
-#./sysrootlibs/freetype/build.sh
-#./sysrootlibs/harfbuzz/build_freetype_nocairo_noglib.sh
-#./sysrootlibs/fontconfig/build.sh
-#./sysrootlibs/pcre2/build.sh
+./sysrootlibs/expat/build.sh
+./sysrootlibs/libffi/build.sh
+./sysrootlibs/wayland/build.sh
+./sysrootlibs/wayland-protocols/build.sh
+./sysrootlibs/pixman/build.sh
+./sysrootlibs/zlib/build.sh
+./sysrootlibs/png/build.sh
+./sysrootlibs/xml2/build.sh
+./sysrootlibs/xkeyboard-config/build.sh
+./sysrootlibs/xkbcommon/build.sh
+./sysrootlibs/icu/build.sh
+./sysrootlibs/harfbuzz/build_nofreetype_nocairo_noglib.sh
+./sysrootlibs/freetype/build.sh
+./sysrootlibs/harfbuzz/build_freetype_nocairo_noglib.sh
+./sysrootlibs/fontconfig/build.sh
+./sysrootlibs/pcre2/build.sh
 ./sysrootlibs/glib/build.sh
-#./sysrootlibs/jpeg/build.sh
-#./sysrootlibs/cairo/build.sh
-#./sysrootlibs/harfbuzz/build.sh
-#./sysrootlibs/upoll/build.sh
-#./sysrootlibs/cygepoll/build.sh
-#./sysrootlibs/fribidi/build.sh
-#./sysrootlibs/tiff4/build.sh
-#./sysrootlibs/pango/build.sh
-#./sysrootlibs/gdk-pixbuf/build.sh
-#./sysrootlibs/egl-stub/build.sh
-#./sysrootlibs/glesv2-stub/build.sh
-#./sysrootlibs/epoxy-emscripten/build.sh
-#./sysrootlibs/graphene/build.sh
-#./sysrootlibs/sass/build.sh
-#./sysrootlibs/sassc/build.sh
-#./sysrootlibs/uapi-stub/build.sh
-#./sysrootlibs/gtk4/build.sh
+./sysrootlibs/jpeg/build.sh
+./sysrootlibs/cairo/build.sh
+./sysrootlibs/harfbuzz/build.sh
+./sysrootlibs/upoll/build.sh
+./sysrootlibs/cygepoll/build.sh
+./sysrootlibs/fribidi/build.sh
+./sysrootlibs/tiff4/build.sh
+./sysrootlibs/pango/build.sh
+./sysrootlibs/gdk-pixbuf/build.sh
+./sysrootlibs/egl-stub/build.sh
+./sysrootlibs/glesv2-stub/build.sh
+./sysrootlibs/epoxy-emscripten/build.sh
+./sysrootlibs/graphene/build.sh
+./sysrootlibs/sass/build.sh
+./sysrootlibs/sassc/build.sh
+./sysrootlibs/uapi-stub/build.sh
+./sysrootlibs/gtk4/build.sh
