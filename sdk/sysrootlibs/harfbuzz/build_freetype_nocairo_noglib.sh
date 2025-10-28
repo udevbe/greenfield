8dbc2a146e916fbf60964e92f1e1386cc080e504
Erik De Rijcke <erik.derijcke@cegeka.com>
2025-10-28 21:56:10 +0100

    Replace `PKG_CONFIG_PATH` export with sourcing `sysroot-env.sh` in HarfBuzz build scripts.

--- sdk/sysrootlibs/harfbuzz/build_freetype_nocairo_noglib.sh
+++ sdk/sysrootlibs/harfbuzz/build_freetype_nocairo_noglib.sh
@@ -21,7 +21,7 @@ ensure_repo() {
 build() {
     ensure_repo
     source ../../emsdk/emsdk_env.sh
-    export PKG_CONFIG_PATH="$_SDK_DIR/sysroot/lib/pkgconfig:$_SDK_DIR/sysroot/share/pkgconfig"
+    source "$_SDK_DIR/sysrootlibs/sysroot-env.sh"
     pushd repo
     	meson setup --wipe build/ --cross-file "${_SDK_DIR}/sysrootlibs/emscripten-toolchain.ini" --cross-file "${_SDK_DIR}/sysrootlibs/emscripten-build.ini" \
     	  -Dfreetype=enabled -Dtests=disabled -Dicu=enabled -Dcairo=disabled -Dintrospection=disabled -Ddocs=disabled -Dutilities=disabled -Dglib=disabled
